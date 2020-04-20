3fff3d2aaced2da641693ed44e76389a6f2662dd
Rakhat <51375666+zethuman@users.noreply.github.com>
2020-04-20 14:18:25 +0600

    bags fixed

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
+import json
+from functools import reduce
 import settings
 import patterns
 import datetime
 import telegramcalendar
 import logging
-from mongodb import mdb, search_or_save_user, search_user_task, list_of_tasks
+from bson import ObjectId
+from mongodb import mdb, search_or_save_user, search_or_save_task
 import logging
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup,
                       ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, ParseMode)
@@ -20,11 +23,11 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 def start(bot, update):
     user = search_or_save_user(mdb, update.effective_user, update.message)
     print(user)
-    reply_keyboard = [["Create Task", "List All Tasks"]]
+    reply_keyboard = [["Create Task", 'List All Tasks'], ["Help", "Tutorial"], ["Restart"]]
     msg = 'Trust in me, (poiled by Rakhat)' + str(update.message.from_user.first_name) + patterns.start_text
     update.message.reply_text(msg, reply_markup=ReplyKeyboardMarkup(
         reply_keyboard,
-        one_time_keyboard=True))
+        resize_keyboard=True))
     return GET_COMMAND
 
 
@@ -55,18 +58,16 @@ def get_username(update):
     return update.effective_user.username
 
 
-# def get_tasks_list(user):
-#     # tasks = Task.objects.filter(reporter=user)
-#     keyboard = [[InlineKeyboardButton(task.title, callback_data=task.id)]
-#                 for task in tasks]
-#     keyboard_markup = InlineKeyboardMarkup(keyboard)
-#     return keyboard_markup
+def get_tasks_list(user):
+    tasks = mdb.tasks.find({'user_id': user.id}, {'task_title': 1,'deadline_data': 1, 'notification_date':1, '_id': 1})
+    keyboard = [[InlineKeyboardButton(task['task_title'], callback_data=str(task['_id']))] for task in tasks]
+    keyboard_markup = InlineKeyboardMarkup(keyboard)
+    return keyboard_markup
 
 
 def list_tasks(bot, update, user_data):
     user = update.message.from_user
     reply_markup = ReplyKeyboardRemove()
-    user = search_or_save_user(mdb, update.effective_user, update.message)
     message = update.message.reply_text(
         "Getting tasks...", reply_markup=reply_markup
     )
@@ -95,12 +96,10 @@ def calendar_handler(bot, update):
 
 def deadline_handler(bot, update, user_data):
     selected, date = telegramcalendar.process_calendar_selection(bot, update)
-
+    print(type(date))
     user_data['deadline_date'] = date
+    user_data['updated_date'] = date
     print(date)
-    # task = user_data["task"]
-    # task.due_date = date
-    # task.save(update_fields=["due_date"])
     reply_keyboard = [["Yes", "No"]]
     bot.send_message(chat_id=update.callback_query.from_user.id,
                      text=f"""You selected {date}.
@@ -131,6 +130,7 @@ def notification_handler(bot, update, user_data):
 def add_notification_date(bot, update, job_queue, user_data):
     notification_time = update.message.text
     notification_date = user_data["notification_date"]
+    user_data['task_status'] = True
     try:
         hour, minutes = map(int, notification_time.split(":"))
         notification = datetime
@@ -140,20 +140,6 @@ def add_notification_date(bot, update, job_queue, user_data):
         )
         return None
 
-    # task = user_data["task"]
-    # task.notification = notification
-    # task.save(update_fields=["notification"])
-    chat_id = update.message.chat_id
-    # job_queue.run_once(alarm, notification, context={
-    #     "chat_id": chat_id,
-    #     "task_data": f"""You have a task {task.title} {task.description}
-    #                  to do before {task.due_date}!"""
-    # })
-    # {notification:%A}, {notification}
-    # update.message.reply_text(
-    #     f"""Thanks! I'll send you a notification
-    #     on your task on """)
-
     text = """\nResults: 
     <b>Task: </b> {task_title}
     <b>Deadline: </b> {deadline_date}
@@ -162,35 +148,122 @@ def add_notification_date(bot, update, job_queue, user_data):
     update.message.reply_text(f"""Thanks! I'll send you a notification
         on your task on \n""" + text, parse_mode=ParseMode.HTML)
     user = search_or_save_user(mdb, update.effective_user, update.message)
-    task = search_user_task(mdb, user, user_data)
-    return ConversationHandler.END
+    task = search_or_save_task(mdb, update.effective_user, user_data)
+    # return ConversationHandler.END
+    return RESTART
 
 
 def task_view(bot, update, user_data):
-    task_id = update.callback_query.data
-    # task = Task.objects.get(id=task_id)
+    taskId = update.callback_query.data
+    oid = ObjectId(taskId)
+    user_data['oid'] = ObjectId(taskId)
+    print(repr(oid))
+    task = mdb.tasks.find_one({'_id': ObjectId(taskId)})
+    print(task)
     reply_keyboard = [["Edit", "Delete"]]
-    user_data["task"] = task
+    # title = task['task_title']
+    # deadline = task['deadline_date']
+    # notification = task['notification_date']
+    if task['task_status'] == True:
+        status = 'Undone'
+    else:
+        status = 'Done'
+    # print(title, deadline)
+
     bot.send_message(chat_id=update.callback_query.from_user.id,
-                     text=f"Task {task.title}\nDescription: "
-                     f"{task.description}\n"
-                     f"Due date: {task.due_date}\nNotification: "
-                     f"{task.notification}\n"
-                     f"Status: {task.status}",
-                     reply_markup=ReplyKeyboardMarkup(
-                         reply_keyboard,
-                         one_time_keyboard=True)
-                     )
+                         text=f"Task {task['task_title']}\n"
+                         f"Due date: {task['deadline_date']}\nNotification: "
+                         f"{task['notification_date']}\n"
+                         f"Status: {status}",
+                         reply_markup=ReplyKeyboardMarkup(
+                             reply_keyboard,
+                             one_time_keyboard=True, resize_keyboard= True)
+                         )
     return SELECT_ACTION
 
 
+def get_task_fields():
+    doc = reduce( lambda all_keys, rec_keys: all_keys | set(rec_keys), map(lambda d: d.keys(), mdb.tasks.find({}, {'_id':0, 'user_id':0})), set() )
+    for key in doc:
+        print(key)
+    keyboard = [[InlineKeyboardButton(name, callback_data=name)]
+                for name in doc]
+    keyboard_markup = InlineKeyboardMarkup(keyboard)
+    return keyboard_markup
 
-# def inline_handler(bot,update):
-#     selected,date = telegramcalendar.process_calendar_selection(bot, update)
-#     if selected:
-#         bot.send_message(chat_id=update.callback_query.from_user.id,
-#                         text="You selected %s" % (date.strftime("%d/%m/%Y")),
-#                         reply_markup=ReplyKeyboardRemove())
+
+def list_edit_options(bot, update, user_data):
+    update.message.reply_text(
+        "Choose a property to edit:", reply_markup=get_task_fields())
+    return GET_EDIT_ACTION
+
+
+def get_edit_action(bot, update, user_data):
+    field = update.callback_query.data
+    user_data["edit"] = field
+    keyboard = [['Done', 'Undone']]
+    if field == 'task_title':
+        bot.send_message(chat_id=update.callback_query.from_user.id,
+                         text=f"Please, send a new value for the task`s title.")
+    if field == 'task_status':
+        bot.send_message(chat_id=update.callback_query.from_user.id,
+                         text=f"Please, send a new value for the task`s status", reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True))
+    elif field == 'deadline_date':
+        bot.send_message(chat_id=update.callback_query.from_user.id,
+                         text=f"Please, send a new value for the task`s deadline in format month/day/year h:m:s")
+    elif field == 'notification_date':
+        bot.send_message(chat_id=update.callback_query.from_user.id,
+                         text=f"Please, send a new value for the task`s notification in format month/day/year h:m:s")
+    return GET_NEW_VALUE
+
+
+
+def edit_task(bot, update, user_data):
+    field = user_data["edit"]
+    oid = user_data['oid']
+    if field == 'task_title':
+        value = update.message.text
+    elif field == 'task_status':
+        handler = update.message.text
+        if handler == 'Undone':
+            value = True
+        elif handler == 'Done':
+            value = False
+    elif field == 'deadline_date':
+        datetime_str = update.message.text
+        value = datetime.datetime.strptime(datetime_str, '%m/%d/%y %H:%M:%S')
+    elif field == 'notification_date':
+        datetime_str = update.message.text
+        value = datetime.datetime.strptime(datetime_str, '%m/%d/%y %H:%M:%S')
+    try:
+        mdb.tasks.update_one({'_id': ObjectId(oid)},
+                        {'$set': {field: value}})
+    except ValueError:
+        update.message.reply_text("Please send a valid value")
+        return
+    if field == 'task_title':
+        update.message.reply_text(f"The task title is updated to {value}")
+    if field == 'dateline_data':
+        update.message.reply_text(f"The task deadline is updated to {value}")
+    if field == 'notification_data':
+        update.message.reply_text(f"The task deadline is updated to {value}")
+    if field == 'task_status':
+        update.message.reply_text(f"The task status is changed to {value}")
+    user_data.clear()
+    # return ConversationHandler.END
+    return RESTART
+
+
+def delete_task(bot, update, user_data):
+    oid = user_data['oid']
+    mdb.tasks.remove({'_id': ObjectId(oid)}, 1)
+    user_data.clear()
+    update.message.reply_text(
+        f"The task is deleted.",
+        reply_markup=ReplyKeyboardRemove()
+    )
+    # return ConversationHandler.END
+    return RESTART
 
 def cancel(bot, update):
     update.message.reply_text(
@@ -227,8 +300,9 @@ def tutorial(bot, update):
     TASK_VIEW,
     SELECT_ACTION,
     GET_EDIT_ACTION,
-    GET_NEW_VALUE
-) = range(11)
+    GET_NEW_VALUE,
+    RESTART
+) = range(12)
 
 
 def main():
@@ -246,6 +320,9 @@ def main():
             GET_COMMAND: [
                 RegexHandler("^Create Task$", add_task),
                 RegexHandler("^List All Tasks$", list_tasks, pass_user_data=True),
+                RegexHandler("^Help$", help),
+                RegexHandler("^Tutorial$", tutorial),
+                RegexHandler("^Restart$", start),
             ],
             TASK_CREATE: [
                 MessageHandler(
@@ -293,6 +370,31 @@ def main():
                     task_view,
                     pass_user_data=True)
             ],
+            SELECT_ACTION: [
+                RegexHandler(
+                    "^Edit$",
+                    list_edit_options,
+                    pass_user_data=True),
+                RegexHandler(
+                    "^Delete$",
+                    delete_task,
+                    pass_user_data=True),
+
+            ],
+            GET_EDIT_ACTION: [
+                CallbackQueryHandler(
+                    get_edit_action,
+                    pass_user_data=True)
+            ],
+            GET_NEW_VALUE: [
+                MessageHandler(
+                    Filters.text,
+                    edit_task,
+                    pass_user_data=True),
+            ],
+            RESTART: [
+                start
+            ]
         },
         fallbacks=[
             CommandHandler("cancel", cancel),
--- mongodb.py
+++ mongodb.py
@@ -17,19 +17,15 @@ def search_or_save_user(mdb, effective_user, message):
     return user
 
 
-def search_user_task(mdb, user, user_data):
-    mdb.users.update_one(
-        {'_id': user['_id']},
-        {'$set': {'task': {'task_title': user_data['task_title'],
-                           'deadline_date': user_data['deadline_date'],
-                           'notification_date': user_data['notification_date']}
-                  }
-        }
-    )
-    return user
+def search_or_save_task(mdb, effective_user, user_data):
+    task = {
+        "user_id": effective_user.id,
+        'task_title': user_data['task_title'],
+        'deadline_date': user_data['deadline_date'],
+        'notification_date': user_data['notification_date'],
+        'task_status': user_data['task_status']
+    }
+    mdb.tasks.insert_one(task)
+    return task
 
 
-def list_of_tasks(mdb, user):
-    for x in user.find():
-        return x
-        print(x)
